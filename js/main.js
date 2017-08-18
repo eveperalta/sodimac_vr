5422e1badbeac76b22c4883b359648207d5f6f8b
eveperalta <eperalta.cm@gmail.com>
2017-08-18 15:01:11 -0300

    js modal no kb

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$('#myModal').modal()                      
+$('#myModal').modal({ keyboard: false })   // initialized with no keyboard
+$('#myModal').modal('show')                // initializes and invokes show immediately
